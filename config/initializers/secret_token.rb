dcf07695edebd7d76c5dd08f900462daeebc2ba2
Drew Skwarcan <drew@weblogicnow.com>
2012-07-11 16:53:50 -0600

    created the new rails application codemap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemap::Application.config.secret_token = '25fb6847130c84cfe9c9311c2b14392b962b457f2d636ae5eb545aed71d21326609ce13c4e573ee0f4ae77642c08b5d8667d1628b4f564a12dca872d2a5235f5'
