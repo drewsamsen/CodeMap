e0366b486ab592d89d22fbd41d2b80144ac09fe9
Drew Skwarcan <drew@weblogicnow.com>
2012-08-09 18:05:19 -0400

    Added arrow icons next to column headers on homepage to indicate current sort column and direction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,8 +14,9 @@ module ApplicationHelper
   # sorted table, like on the note's index page
   def sortable(column, title = nil)
   	title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
   	direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
   	link_to title, {:sort => column, :direction => direction},
-  				   {:title => "Sort by #{title}"}
+  				   {:title => "Sort by #{title}", :class => css_class}
   end
 end
